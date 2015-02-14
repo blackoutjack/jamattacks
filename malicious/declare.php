73932f55de8f5820501b1ccf1d56168b9aff9da0
Rich Joiner <joiner@cs.wisc.edu>
2015-02-14 14:38:33 -0600

    Global settings updates

--- malicious/declare.php
+++ malicious/declare.php
@@ -3,13 +3,16 @@
 session_start();
 
 $hostname = $_SERVER['HTTP_HOST'];
-if ($hostname == 'policy-weaving.cs.wisc.edu') {
+if ($hostname === 'blackoutjack.com' || $hostname === 'www.blackoutjack.com') {
   define('MALHOST', 'http://blackoutjack.com/malicious/');
   define('TGTHOST', 'https://policy-weaving.cs.wisc.edu/target/');
+
+  define('INCDIR', implode(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'malicious', 'inc', '')));
 } else {
   define('MALHOST', 'http://malicious/');
   define('TGTHOST', 'http://target/');
+
+  define('INCDIR', implode(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'inc', '')));
 }
 
-define('INCDIR', $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'inc'.DIRECTORY_SEPARATOR);
 ?>
--- target/declare.php
+++ target/declare.php
@@ -3,14 +3,14 @@
 session_start();
 
 $hostname = $_SERVER['HTTP_HOST'];
-if ($hostname == 'policy-weaving.cs.wisc.edu') {
+if ($hostname === 'policy-weaving.cs.wisc.edu') {
   define('MALHOST', 'http://blackoutjack.com/malicious/');
   define('TGTHOST', 'https://policy-weaving.cs.wisc.edu/target/');
   if (mysql_connect("mysql.cs.wisc.edu", "policy_weaver", "crash")) {
     mysql_select_db("policy_weaving");
   }
 
-  define('INCDIR', implode(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'attacks', 'inc', '')));
+  define('INCDIR', implode(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'target', 'inc', '')));
 } else {
   define('MALHOST', 'http://malicious/');
   define('TGTHOST', 'http://target/');
