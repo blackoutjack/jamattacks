df0bad62e74ddeb36855da597f60832a89a7cd74
Rich Joiner <joiner@cs.wisc.edu>
2015-02-16 15:31:06 -0600

    Fix up xml

--- malicious/submission.php
+++ malicious/submission.php
@@ -1,13 +1,21 @@
 <?php
-include('../declare.php');
+include('declare.php');
 
 // Enable CORS access
 header('Access-Control-Allow-Origin: *');
+header('Content-type: text/xml; charset=utf-8');
+
+$doc = new DomDocument('1.0', 'UTF-8');
+$resp = $doc->createElement('response');
+
+$from = $doc->createElement('from', $_SERVER['HTTP_HOST']);
+$resp->appendChild($from);
 
 $testname = isset($_REQUEST['test']) ? $_REQUEST['test'] : false;
+$test = $doc->createElement('test', $testname);
+$resp->appendChild($test);
+
+$doc->appendChild($resp);
+
+print $doc->saveXML();
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
-<response>
-<from><?=htmlspecialchars($_SERVER['HTTP_HOST'])?></from>
-<test><?=htmlspecialchars($testname)?></test>
-</response>
